6c119cd26622cb7090c9c156e56cee4436b8da00
Beatriz Huberto <169067859+trizrto@users.noreply.github.com>
2024-09-09 10:46:12 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,25 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Programa feito para calcular a velocidade média em km/h");
+
+    System.out.println("");
+    System.out.println("");
+
+    System.out.println("A distância considerada é: ");
+    double distancia = 400;
+    System.out.println(distancia);
+    System.out.println("");
+    System.out.println("O tempo usado é igual a:");
+    double tempo = 9;
+    System.out.println(tempo);
+    System.out.println("");
+System.out.println("a velocidade media é igual a:");
+    double velocidadeMedia = distancia / tempo; 
+    System.out.println(velocidadeMedia);
+    
+  }
+
+  
+}
